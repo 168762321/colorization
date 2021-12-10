f91fe2710d1afe205dfc3820b3e7be431ef7d080
zhangjw <168762321@qq.com>
2021-12-10 12:09:08 +0800

    修复提交任务页面导入不能使用多次bug

--- project/main.py
+++ project/main.py
@@ -1,5 +1,4 @@
-from sqlalchemy import sql
-from sqlalchemy.sql.expression import false, true
+from sqlalchemy.sql.expression import false, text, true
 from service import * 
 import log
 import dearpygui.dearpygui as dpg
@@ -40,91 +39,87 @@ def ffmpeg_py(video_path):
         probe = ffmpeg.probe(video_path)
     except ffmpeg.Error as e:
         print(e.stderr, file=sys.stderr)
-        popups('ffmpeg_error',"视频解析错误，检查视频是否可以正常播放！！")
+        popups('ffmpeg_error',"视频解析错误，检查视频是否可以正常播放!!")
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     if video_stream is None:
-        popups('ffmpeg_no_stream_error',"没有找到视频流信息，请检查视频文件！！")
+        popups('ffmpeg_no_stream_error',"没有找到视频流信息，请检查视频文件!!")
     num_frames = int(video_stream['nb_frames'])
     fps = video_stream['avg_frame_rate'].split('/')[0]            
     return num_frames,fps
 
-
-#创建任务回调函数
-def save_callback(sender, app_data, user_data):
-    name = dpg.get_value('task_name_input')
-    try:
-        if name:
-            print('name_input value：',name)
-            session.query(Project).filter(Project.name == name).first()
-            file_select()  #跳转上传页面
-            global task_name
-            task_name = name
-    except Exception as e:
-        log.write(e)
 
 def select_sql_task_name(sender,app_data,user_data):
     print(sender,app_data,user_data)
     if app_data :
         sql_name=session.query(Project).filter(Project.name == app_data).first()
         if sql_name:
-            dpg.set_value('task_tip','存在重复名称，不能创建任务')
+            dpg.set_value('task_tip','存在重复名称，不能创建任务.')
         else:
-            dpg.set_value('task_tip','名称通过，可以创建任务')
+            dpg.set_value('task_tip','名称通过，可以创建任务.')
     else:
-        dpg.set_value('task_tip','名称不能是空，不能创建任务')
+        dpg.set_value('task_tip','名称不能是空，不能创建任务.')
+
 
 #删除任务
 def _delete_task(sender, app_data, user_data):
     delete_id = user_data
     try:
         if delete_id:
+            info = session.query(Project).filter(Project.id==delete_id).first()
             session.query(Project).filter(Project.id == delete_id).delete()
             session.commit()
             session.query(Shortcut).filter(Shortcut.project_id == delete_id).delete()
             session.commit()
-            log.write("删除任务id："+str(delete_id))
+            log.write("删除任务id:"+str(delete_id))
+            # 删除文件
+            delete_task_all_files(info.project_path)
+            print("delete all task files")
     except Exception as e:
         print(e)
         log.write(e)
 
 
+#删除任务同时删除文件夹下全部内容
+def delete_task_all_files(file_path):
+    for i in os.listdir(file_path):
+        file_data = os.path.join(file_path,i)
+        print(file_data)
+        if os.path.isfile(file_data) == True:#os.path.isfile判断是否为文件,如果是文件,就删除.如果是文件夹.递归给自己.
+            os.remove(file_data)
+        else:
+            delete_task_all_files(file_data)
+    os.rmdir(file_path)
+
+
 #导入视频回调
 def video_file_select_callback(sender, app_data):
+    input_task_name = dpg.get_value('task_name_input')
+    # print(input_task_name)
     project_path = os.path.join(os.getcwd(), "task")#创建文件夹
-    project_path = os.path.join(project_path, task_name)
+    project_path = os.path.join(project_path, input_task_name)
     create_file(project_path)
     video_path = app_data['file_path_name']
     frame_path = os.path.join(os.getcwd(), "task")
-    frame_path = os.path.join(frame_path, task_name)
+    frame_path = os.path.join(frame_path, input_task_name)
     frame_path = os.path.join(frame_path, "allframe")
     frame_max_index, video_fps= ffmpeg_py(app_data['file_path_name'])
     global all_frame_num
     all_frame_num = frame_max_index #更新全局变量为本视频全部帧
     # create_time = sql_time
-    save_p_info= Project(name=task_name,project_path = project_path ,video_path= video_path,
+    save_p_info= Project(name=input_task_name,project_path = project_path ,video_path= video_path,
                         video_fps= video_fps,frame_path= frame_path, frame_min_index=1,
                         frame_max_index= frame_max_index,create_time=sql_time,update_time=sql_time)
     session.add(save_p_info)
     session.commit()
-    p_task_id = session.query(Project.id).filter(Project.name == task_name).all()
+    p_task_id = session.query(Project.id).filter(Project.name == input_task_name).all()
     global task_id
     task_id = p_task_id[0][0]
-    info = "创建任务成功，名称："+str(task_name)
+    info = "创建任务成功，名称:"+str(input_task_name)
     log.write(info)
     print('任务创建完成')
-    print('name:{},project_path:{},video_path:{},video_fps:{},frame_path:{},frame_max_index:{},create_time:{}'.format(task_name,project_path,video_path,video_fps,frame_path,frame_max_index,sql_time))
+    print('name:{},project_path:{},video_path:{},video_fps:{},frame_path:{},frame_max_index:{},create_time:{}'.format(input_task_name,project_path,video_path,video_fps,frame_path,frame_max_index,sql_time))
     Primary_Window()
 
-
-#上传窗口弹出,不支持中文文件。0表示视频上传，1表示图像上传
-def file_select():
-    with dpg.file_dialog(directory_selector=False, show=False, callback=video_file_select_callback, tag="video_file_dialog_tag"):
-        dpg.add_file_extension(".mp4", color=(0, 255, 0, 255))
-        dpg.add_file_extension(".ts", color=(0, 255, 0, 255))
-        dpg.add_file_extension(".avi", color=(0, 255, 0, 255))
-        dpg.add_file_extension(".mov", color=(0, 255, 0, 255))
-    dpg.configure_item("video_file_dialog_tag",show=True)
-   
 
 #已经创建任务回调跳转
 def changge_win(sender, app_data, user_data):
@@ -148,11 +143,16 @@ def Colorization():
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
     with dpg.window(label="Colorization Window",width=800,height=600,id='Colorization Window'):
         dpg.bind_font(font1) 
+        with dpg.file_dialog(directory_selector=False, show=False, callback=video_file_select_callback, tag="video_file_dialog_tag"):
+            dpg.add_file_extension(".mp4", color=(0, 255, 0, 255))
+            dpg.add_file_extension(".ts", color=(0, 255, 0, 255))
+            dpg.add_file_extension(".avi", color=(0, 255, 0, 255))
+            dpg.add_file_extension(".mov", color=(0, 255, 0, 255))
         with dpg.group(horizontal=True):
-            dpg.add_button(label="编辑创建任务",callback=save_callback)
+            dpg.add_button(label="编辑创建任务",callback=lambda: dpg.show_item("video_file_dialog_tag"))
             dpg.add_input_text(label="", default_value="请输入任务名称",tag="task_name_input",callback=select_sql_task_name)
         with dpg.group(horizontal=True):
-            dpg.add_text(default_value="检测提示：")
+            dpg.add_text(default_value="检测提示:")
             dpg.add_text(default_value='请输入任务名称',id="task_tip")
 
         with dpg.table(label='task_list',header_row=True, resizable=True, policy=dpg.mvTable_SizingStretchProp,
@@ -161,10 +161,13 @@ def Colorization():
             dpg.add_table_column(label="操作")
             #查询数据库
             name_list = search_Project_all()
+            print(name_list)
             table_num = len(name_list)
             for i in range(0,table_num):
                 with dpg.table_row():
-                    dpg.add_button(label=f"{name_list[i][1]}" , user_data=name_list[i][0] , callback=changge_win)  #回调没写，创建下一级页面，传递当前的user_data 给数据库。
+                    dpg.add_button(label=f"{name_list[i][1]}" , user_data=name_list[i][0] , callback=changge_win,)  #回调没写，创建下一级页面，传递当前的user_data 给数据库。
+                    # dpg.add_button(label=f"{name_list[i][1]}" , user_data=name_list[i][0] , callback=_log,)  #回调没写，创建下一级页面，传递当前的user_data 给数据库。
+
                     dpg.add_button(label="X" , user_data=name_list[i][0] ,callback=_delete_task )
 
 
@@ -177,28 +180,29 @@ def Primary_Window():
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
-    win_name = "Primary_Window——当前处理任务："+task_name
+    win_name = "Primary_Window——当前处理任务:"+task_name
     with dpg.window(label=win_name,width=800,height=800,id='Primary Window'):
         dpg.bind_font(font1)
         with dpg.menu_bar():
             with dpg.menu(label="工具"):
                 dpg.add_menu_item(label="视频转图像", callback=video_to_png)
                 dpg.add_menu_item(label="自动分镜头", callback=_log)
+            with dpg.menu(label="导出"):
+                dpg.add_menu_item(label="导出", callback=_log)
         with dpg.child_window(label='all_frame_image',height=500,parent='Primary Window'):
             dpg.add_text("全部帧图像")
             with dpg.plot(label="Drag Lines/Points", height=400, width=-1):
                 dpg.add_plot_legend()
-                dpg.add_plot_axis(dpg.mvXAxis, label="时间",no_gridlines=True)
-                # dpg.add_plot_axis(dpg.mvYAxis, label="y")
+                dpg.add_plot_axis(dpg.mvXAxis, label="时间")
+                dpg.add_plot_axis(dpg.mvYAxis)
                 dpg.add_drag_line(label="dline1", color=[255, 0, 0, 255],callback=_log)
 
 
-
-            # width, height, channels, data = dpg.load_image("data\Somefile.png")
-            # with dpg.texture_registry(show=True):
-            #     dpg.add_static_texture(width, height, data, tag="texture_tag")
-            # dpg.add_image("texture_tag",width=170,height=140)
-            # dpg.add_image("texture_tag",width=170,height=140)
+                # width, height, channels, data = dpg.load_image("data\Somefile.png")
+                # with dpg.texture_registry(show=True):
+                #     dpg.add_static_texture(width, height, data, tag="texture_tag")
+                # dpg.add_image("texture_tag",width=170,height=140)
+                # dpg.add_image("texture_tag",width=170,height=140)
         with dpg.child_window(label='child_frame_image',height=300,parent='Primary Window'):
             dpg.add_text("分镜头图像")    
     
@@ -220,7 +224,7 @@ def Primary_Window():
                 dpg.add_text('点击上传参考帧')
                 dpg.add_button(label="上传",callback=lambda: dpg.show_item("file_dialog_tag"))
                 with dpg.group(horizontal=True):
-                    dpg.add_text('文件地址：')
+                    dpg.add_text('文件地址:')
                     dpg.add_text('空',id='file_path_name')
             with dpg.group(horizontal=True):
                 dpg.add_text('确认是否满足上色条件')
@@ -296,6 +300,7 @@ def video_to_png():
     try:
         popups('video_info',"转换视频图片中，耐心等待。")
         with dpg.window(label='video_progress_bar',show=True, no_title_bar=True,id = 'video_progress_bar'):
+            dpg.add_loading_indicator()
             dpg.add_text('转换进度条')
             dpg.add_progress_bar(label="Progress Bar", default_value=78.00, overlay="78.00%",tag='vid_to_png_progress',width=200)
             dpg.add_text(label="Progress Bar")
@@ -310,13 +315,49 @@ def video_to_png():
         print(e)
         log.write(e)
 
-
+def Synthetic_video():
+    with dpg.font_registry(): #注册字体，自选字体
+        with dpg.font(CN_font, 15) as font1:	#增加中文编码范围，防止问好
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
+    with dpg.window(label="Synthetic_video",width=500,height=350,id='Synthetic_video'):
+        dpg.bind_font(font1)
+        with dpg.child_window(label='radio group',height=300,parent='Synthetic_video'):
+            with dpg.group(horizontal=True):
+                dpg.add_text("作品名称")
+                dpg.add_input_text(default_value='xxxxx')
+            with dpg.group(horizontal=True):
+                dpg.add_text('分辨率')
+                dpg.add_radio_button(("480p","720p","1080p",'2k','4k'), default_value="1080p",callback=_log, horizontal=True)
+            with dpg.group(horizontal=True):
+                dpg.add_text('码率')
+                dpg.add_radio_button(['更低','推荐','更高'], default_value='推荐',callback=_log, horizontal=True)
+            with dpg.group(horizontal=True):
+                dpg.add_text('编码')
+                dpg.add_radio_button(['H.264','HEVC'], default_value='H.264',callback=_log, horizontal=True)
+            with dpg.group(horizontal=True):
+                dpg.add_text('格式')
+                dpg.add_radio_button(['mp4'], default_value='mp4',callback=_log, horizontal=True)
+            with dpg.group(horizontal=True):
+                dpg.add_text('帧率fps')
+                dpg.add_radio_button([25,30,60], default_value=30,callback=_log, horizontal=True)
+            
+            # with dpg.group(horizontal=True):
+            #     dpg.add_text('编码')
+            #     dpg.add_button(label="上传",callback=lambda: dpg.show_item("file_dialog_tag"))
+            #     with dpg.group(horizontal=True):
+            #         dpg.add_text('文件地址:')
+            #         dpg.add_text('空',id='file_path_name')
+           
+            dpg.add_button(label='导出',id='final_submit',callback=_log )
 
 
 
 if __name__=='__main__':
     dpg.create_context()
     Colorization()
+    Synthetic_video()
     dpg.create_viewport(title='Colorization_sys window ', width=900, height=800)
     dpg.setup_dearpygui()
     dpg.show_viewport()
